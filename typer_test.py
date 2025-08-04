a6bc50ca35f92a5ac3c6517aaeab53dbe707a599
Jobinjohn2002 <jobin.john@excelenciaconsulting.com>
2025-08-04 15:34:24 +0530

    Delete typer_test.py

--- typer_test.py
+++
@@ -1,8 +0,0 @@
-user_input = input("Enter something: ")
-eval(user_input)
-
-user_input1 = input("Enter something: ")
-eval(user_input1)
-
-user_input2 = input("Enter something: ")
-eval(user_input1)
